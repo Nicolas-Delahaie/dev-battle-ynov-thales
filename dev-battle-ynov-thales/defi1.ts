28c2b3927ab059e360162bda32102c8897bde9a7
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2024-10-16 00:12:30 +0200

    Isolation dans des fonctions anonymes pour ne pas interferer entre les fichiers

--- dev-battle-ynov-thales/defi1.ts
+++ dev-battle-ynov-thales/defi1.ts
@@ -1,41 +1,45 @@
 "use strict";
+// https://tainix.fr/sandbox/play/GREENOID_4
 
-// NE PAS TOUCHER
-const co2: number[] = [62, 73, 53, 51, 74, 90, 83, 57, 86, 82, 63, 60];
-const water: number[] = [406, 536, 656, 758, 904, 638, 863, 851, 883, 473, 732];
-const deforestation: number[] = [6505, 8857, 9385, 9353, 8411, 5002, 8759, 9543];
-const agricultural: number[] = [12, 9, 15, 9, 12, 13, 14, 5, 5];
-const plastic: number[] = [3100, 2504, 2232, 1926, 2428, 849, 2086, 2994, 2304];
-const renewable: number[] = [14, 14, 17, 18, 5, 7, 18, 11, 12, 17];
-// NE PAS TOUCHER
+(() => {
+  // NE PAS TOUCHER
+  const co2: number[] = [62, 73, 53, 51, 74, 90, 83, 57, 86, 82, 63, 60];
+  const water: number[] = [406, 536, 656, 758, 904, 638, 863, 851, 883, 473, 732];
+  const deforestation: number[] = [6505, 8857, 9385, 9353, 8411, 5002, 8759, 9543];
+  const agricultural: number[] = [12, 9, 15, 9, 12, 13, 14, 5, 5];
+  const plastic: number[] = [3100, 2504, 2232, 1926, 2428, 849, 2086, 2994, 2304];
+  const renewable: number[] = [14, 14, 17, 18, 5, 7, 18, 11, 12, 17];
+  // NE PAS TOUCHER
 
-function popMinMax(measures: number[]) {
-  measures.sort((a, b) => a - b);
-  measures.shift();
-  measures.pop();
 
-  return measures;
-}
+  function popMinMax(measures: number[]) {
+    measures.sort((a, b) => a - b);
+    measures.shift();
+    measures.pop();
 
-function moy(measures: number[]) {
-  measures = popMinMax(measures);
+    return measures;
+  }
 
-  let total = 0;
-  measures.forEach((val) => {
-    total += val;
-  });
+  function moy(measures: number[]) {
+    measures = popMinMax(measures);
 
-  return Math.floor(total / measures.length);
-}
+    let total = 0;
+    measures.forEach((val) => {
+      total += val;
+    });
 
-let co2Moy = moy(co2);
-let waterMoy = moy(water);
-let deforestationMoy = moy(deforestation);
-let agriculturalMoy = moy(agricultural);
-let plasticMoy = moy(plastic);
-let renewableMoy = moy(renewable);
+    return Math.floor(total / measures.length);
+  }
 
-const p = Math.floor(((co2Moy + plasticMoy / 1000) / 2) * (1 - renewableMoy / 100));
-const r = Math.floor((100 - waterMoy / 10 + (100 - deforestationMoy / 100) + agriculturalMoy + renewableMoy) / 4);
+  let co2Moy = moy(co2);
+  let waterMoy = moy(water);
+  let deforestationMoy = moy(deforestation);
+  let agriculturalMoy = moy(agricultural);
+  let plasticMoy = moy(plastic);
+  let renewableMoy = moy(renewable);
 
-console.log(p + "_" + r);
+  const p = Math.floor(((co2Moy + plasticMoy / 1000) / 2) * (1 - renewableMoy / 100));
+  const r = Math.floor((100 - waterMoy / 10 + (100 - deforestationMoy / 100) + agriculturalMoy + renewableMoy) / 4);
+
+  console.log(p + "_" + r);
+})();
--- dev-battle-ynov-thales/defi2.ts
+++ dev-battle-ynov-thales/defi2.ts
@@ -1,53 +1,55 @@
 "use strict";
+// https://tainix.fr/sandbox/play/GREENOID_5
 
-// NE PAS TOUCHER
-const sequences = [
-  "DHDDDHDCPDH",
-  "CHHHCDPHHC",
-  "PDHPHDPHPCDP",
-  "PPHHDCPCPHC",
-  "DPCHPCHHCPC",
-  "DPDDDPCHPCD",
-  "PDCDHHCPCCP",
-  "DHDPDDCPHDD",
-  "PPHCDHCCCCC",
-  "DCCDHDDCCHD",
-];
-const storages = ["DDCCC", "HPPHPH", "PPHCCC", "PCPHC", "HCHD"];
-// NE PAS TOUCHER
+(() => {
+  // NE PAS TOUCHER
+  const sequences = [
+    "DHDDDHDCPDH",
+    "CHHHCDPHHC",
+    "PDHPHDPHPCDP",
+    "PPHHDCPCPHC",
+    "DPCHPCHHCPC",
+    "DPDDDPCHPCD",
+    "PDCDHHCPCCP",
+    "DHDPDDCPHDD",
+    "PPHCDHCCCCC",
+    "DCCDHDDCCHD",
+  ];
+  const storages = ["DDCCC", "HPPHPH", "PPHCCC", "PCPHC", "HCHD"];
+  // NE PAS TOUCHER
 
-const mapping = {
-  P: 1,
-  H: 2,
-  C: 3,
-  D: 4,
-};
 
-function getTotalDuration(seq: string) {
-  let total = 0;
-  seq.split("").forEach((val) => {
-    total += mapping[val];
-  });
-  return total;
-}
+  const mapping = {
+    P: 1,
+    H: 2,
+    C: 3,
+    D: 4,
+  };
 
-function getMax(sequences: string[]) {
-  let max = { index: 0, duration: 0 };
-  sequences.forEach((seq, index) => {
-    const duration = getTotalDuration(seq);
-    if (duration > max.duration) {
-      max = { index, duration };
-      console.log(max);
-    }
-  });
-  //   sequences.reduce((acc, val) => {}) essyer avec reduce
-  return max;
-}
-const maxSequence = getMax(sequences);
-const maxStorage = getMax(storages);
+  function getTotalDuration(seq: string) {
+    let total = 0;
+    seq.split("").forEach((val) => {
+      total += mapping[val];
+    });
+    return total;
+  }
 
-console.log(maxSequence, maxStorage);
-const res =
-  sequences[maxSequence.index] + sequences[maxStorage.index] + "_" + (maxSequence.duration + maxStorage.duration);
+  function getMax(sequences: string[]) {
+    let max = { index: 0, duration: 0 };
+    sequences.forEach((seq, index) => {
+      const duration = getTotalDuration(seq);
+      if (duration > max.duration) {
+        max = { index, duration };
+      }
+    });
 
-console.log(res);
+    return max;
+  }
+  const maxSequence = getMax(sequences);
+  const maxStorage = getMax(storages);
+
+  const res =
+    sequences[maxSequence.index] + storages[maxStorage.index] + "_" + (maxSequence.duration + maxStorage.duration);
+
+  console.log(res);
+})();
